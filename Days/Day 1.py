68bfaa61b8c01536f861e8e3500030dd2539c437
Jahdo <jahdovpool@hotmail.com>
2025-03-31 11:48:41 -0400

    Day 1 of coding

--- Days/Day 1.py
+++
@@ -1,7 +0,0 @@
-print("Welcome to the Band Name Generator!")
-city = input('What is the name of the city you grew up in?\n')
-pet_name = input("What was/is your favorite pet's name?\n")
-band_name = city.capitalize() + ' ' + pet_name.capitalize()
-print("Your band name could be: "+ band_name)
-
-
